938dbf7aa9f03caaef5a3e4abff5c31c012bd643
IgorLBS <108560697+igorbsilva1901@users.noreply.github.com>
2023-06-25 18:06:54 -0300

    Cria Classes "Principais"
    Cria Classes main,pessoa e tabuleiro

---
+++ src/main/Pessoa.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Pessoa {
+	
+	private String nome;
+	private int pontos = 0;
+	
+	public Pessoa(String nome) {	
+		this.nome = nome;
+	}
+	
+
+}
---
+++ src/main/Tabuleiro.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Tabuleiro {
+
+	private int colunas=7,linhas=6;
+	private int[][] matrizPecas=new int[this.linhas][this.colunas];
+	
+	public Tabuleiro() { //Construtor
+		
+		for(int x = 0;x<this.linhas;x++) {
+			for(int y = 0;y<this.linhas;y++) {
+				matrizPecas[x][y] = 0;
+			}
+		}	
+	}
+	
+	public void checaSituacao() {//Verifica se peças podem ou não ser chamadas
+		
+	}
+	
+	public String checaVitoria() {
+		
+		return "String";
+	}
+	
+}
