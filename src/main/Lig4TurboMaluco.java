98e1b3d1fd71f859db711ac0e4852fc4dedaf17f
IgorLBS <108560697+igorbsilva1901@users.noreply.github.com>
2023-07-01 16:17:11 -0300

    Criacao de método turboMalucoAltera
    Separei o método de modificação das peças vizinhas do método alteraTab

--- src/main/Lig4TurboMaluco.java
+++ src/main/Lig4TurboMaluco.java
@@ -6,16 +6,39 @@ public class Lig4TurboMaluco extends Partida {
     
 
     public int alteraTab(int index, int coluna){
-        Random random = new Random();
-        boolean altera;
-        int row, column;
+        
 
         for (int x = 0; x < tab.getLinhas()-1; x++) {
 
             if (tab.getMatrizPecas(x+1, coluna-1) != 0) { // Verifica se a próxima casa da coluna tem alguma peça
                 tab.setMatrizPecas(x, coluna-1, index);
+                this.turboMalucoAltera(1, x, coluna, index);
+                
 
-                for(int k = 0; k < 3;k++){
+                tab.imprimeMatriz();
+                    
+                return 1;
+            }   
+    }
+
+        tab.setMatrizPecas(tab.getLinhas()-1, coluna-1, index);//Se não tiver nenhuma peça na coluna, a peça vai ficar na última linha da coluna
+        this.turboMalucoAltera(2, tab.getLinhas()-1, coluna, index);
+        
+
+        tab.imprimeMatriz();
+        System.out.println("Caso 2");
+        return 1;
+    
+    
+    }
+    
+    public void turboMalucoAltera(int tipo,int x, int coluna, int index){
+        Random random = new Random();
+        boolean altera;
+        int row, column;
+
+        if(tipo==1){
+            for(int k = 0; k < 3;k++){
                     for(int y = 0; y<3;y++){//Cliclos para percorrer todos os vizinhos
                         if(k==0){
 
@@ -24,7 +47,7 @@ public class Lig4TurboMaluco extends Partida {
 
                             altera = random.nextBoolean();//Boolean aleatório para decidir se troca ou não
 
-                            if(altera && tab.getMatrizPecas(row, column) !=0){
+                            if(altera && tab.getMatrizPecas(row, column) !=0){//Se o boolean for true e a peça vizinha não estiver vazia, a troca vai ocorrer
                             
                                 try{
 
@@ -73,20 +96,13 @@ public class Lig4TurboMaluco extends Partida {
                     }
                 }
 
-                tab.imprimeMatriz();
-                    
-                return 1;
-            }   
-    }
-
-        tab.setMatrizPecas(tab.getLinhas()-1, coluna-1, index);//Se não tiver nenhuma peça na coluna, a peça vai ficar na última linha da coluna
-
-        for(int k = 0;k<2;k++){
+        }else if(tipo==2){
+            for(int k = 0;k<2;k++){
 
             for(int y = 0;y<3;y++){//Nesse caso só há peças na mesma linha e em cima
 
                 if(k==0){
-                    row = tab.getLinhas()-1;
+                    row = x;
                     column = coluna-y;
                     altera = random.nextBoolean();
 
@@ -102,8 +118,8 @@ public class Lig4TurboMaluco extends Partida {
                     }}
 
                 }else{
-                    
-                    row = tab.getLinhas()-2;
+
+                    row = x-1;
                     column = coluna-y;
                     altera = random.nextBoolean();
 
@@ -124,14 +140,8 @@ public class Lig4TurboMaluco extends Partida {
         }
 
     }
-
-        tab.imprimeMatriz();
-        System.out.println("Caso 2");
-        return 1;
-    
-    
+        }
     }
-
 }
 
 
