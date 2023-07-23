733b758fe1a181e61e057679d9e3a7e2683c2beb
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-07-23 20:34:12 -0300

    update telas

--- LIG4Jogo/src/com/lig4/gui/TabuleiroGUI.java
+++ LIG4Jogo/src/com/lig4/gui/TabuleiroGUI.java
@@ -9,26 +9,26 @@ import javafx.scene.shape.Circle;
 public class TabuleiroGUI {
     private final int linhaMax = 6;
     private final int colunaMax = 7;
+    private int row = linhaMax - 1;
 
     public void alteraTabGui(GridPane gridPane, int col) {
-        for (int row = linhaMax - 1; row > 0; row--) {
             Circle circuloAtual = getCirculo(gridPane, col-1, row);
             Circle circuloProximo = getCirculo(gridPane, col-1, row + 1);
+
+            System.out.println(row);
 
             if (circuloAtual != null && circuloProximo != null && circuloProximo.getFill() != Color.WHITE) {
                 System.out.println("1");
                 circuloAtual.setFill(Color.RED);
-                break;
+                row--;
             } else if (circuloAtual != null) {
                 System.out.println("2");
-                circuloAtual.setFill(Color.BLUE);
-                break;
+                circuloAtual.setFill(Color.YELLOW);
+                row--;
             } else{
                 System.out.println("3");
-                break;
             }
         }
-    }
 
     public Circle getCirculo(GridPane gridPane, int col, int row) {
         for (Node node : gridPane.getChildren()) {
