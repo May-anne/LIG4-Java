72b7272d632f6bf4d7513ff28fdc0638c0c452d3
IgorLBS <108560697+igorbsilva1901@users.noreply.github.com>
2023-07-26 16:48:52 -0300

    Empilhamento de peças
    Alteração de lógica GUI para empilhamento de peças no tabuleiro

--- LIG4Jogo/src/com/lig4/gui/TabuleiroGUI.java
+++ LIG4Jogo/src/com/lig4/gui/TabuleiroGUI.java
@@ -11,7 +11,7 @@ import javafx.scene.shape.Circle;
 public class TabuleiroGUI {
     private final int linhaMax = 6;
     private final int colunaMax = 7;
-    private int row = linhaMax - 1;
+    private int row;
     private Pessoa jogadorAtual, jogador1, jogador2;
 
     public TabuleiroGUI(Pessoa jogador1, Pessoa jogador2) {
@@ -21,24 +21,31 @@ public class TabuleiroGUI {
     }
 
     public void alteraTabGui(GridPane gridPane, int col) {
-        Circle circuloAtual = getCirculo(gridPane, col - 1, row);
+        Circle circuloAtual;
         Circle circuloProximo = getCirculo(gridPane, col - 1, row + 1);
-    
         System.out.println(row);
-    
-        if (circuloAtual != null && circuloProximo != null && circuloProximo.getFill() != Color.WHITE) {
-            circuloAtual.setFill(jogadorAtual == jogador1 ? Color.RED : Color.YELLOW);
-            row--;
-        } else if (circuloAtual != null) {
-            circuloAtual.setFill(jogadorAtual == jogador1 ? Color.YELLOW : Color.RED);
-            row--;
-        } else {
-            System.out.println("A coluna está cheia!");
+        for(int i = 1; i<=6; i++){
+            row = linhaMax - i;
+
+            circuloAtual = getCirculo(gridPane, col - 1, row);
+
+            if(circuloAtual.getFill().equals(Color.WHITE)){
+
+                circuloAtual.setFill(jogadorAtual == jogador1 ? Color.RED : Color.YELLOW);
+                break;
+
+            } else if(!circuloAtual.getFill().equals(Color.WHITE)){
+                System.out.println("Espaço preenchido, passando pro próximo de cima");
+            }
         }
+        
+    
+       
     }
     
 
     public Circle getCirculo(GridPane gridPane, int col, int row) {
+
         for (Node node : gridPane.getChildren()) {
             if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
                 if (node instanceof StackPane) {
