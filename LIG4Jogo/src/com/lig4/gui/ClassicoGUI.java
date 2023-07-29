e1f73e43b027dc3d19cc0270f3d6e3be2371c48c
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-07-29 16:03:16 -0300

    tela

--- LIG4Jogo/src/com/lig4/gui/ClassicoGUI.java
+++ LIG4Jogo/src/com/lig4/gui/ClassicoGUI.java
@@ -5,8 +5,7 @@ import com.lig4.jogadores.Pessoa;
 import javafx.scene.layout.GridPane;
 
 public class ClassicoGUI extends TabuleiroGUI {
-
-
+    
     public ClassicoGUI(Pessoa jogador1,Pessoa jogador2){
         super(jogador1,jogador2);
     }
